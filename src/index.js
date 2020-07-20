4793ef3674199d0be8c24bcf26804c97d1520d7d
Thiago Demas <thiago.demas7@gmail.com>
2020-07-20 14:01:00 -0300

    Update Readme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const routes = require('./routes');
+const http = require('http');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0.kkcxs.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
-app.listen('3333');
+server.listen('3333');
